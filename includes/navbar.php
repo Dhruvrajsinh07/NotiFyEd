2b129cfb10e729de828dc27e175bbc6874d62199
Dhruvrajsinh-Padhiyar <dhruvrajpadhiyar09@gmail.com>
2025-07-25 15:53:22 +0530

    commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -35,7 +35,7 @@
             <?php
             if($_SESSION['role'] == 'student'){
               ?><li class="nav-item">
-              <a class="nav-link text-purple" href="<?= urlof('./pages/feedback.php');?>">Feedback</a>
+              <a class="nav-link text-purple" href="<?= urlof('./pages/feedback.php');?>">Feedback efwf </a>
             </li><?php
             }
             ?>
