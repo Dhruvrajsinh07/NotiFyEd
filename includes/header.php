8ecfabe680ed1a18f6f3c0e46105332f70cc3f42
Parth-Chavda <parth18chavda@gmail.com>
2025-07-26 15:01:54 +0530

    session added

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,15 @@
-<!DOCTYPE html>
+<?php
+$url = urlof('pages/User/login.php');
+if (!isset($_SESSION['user'])) {
+    header("Location: $url");
+    exit;
+}
+?><!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>All Notices | NotiFyEd</title>
+  <title>NotiFyEd</title>
 
   <!-- Bootstrap 5 CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
