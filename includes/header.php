d2bb55f470abcab1d34b96e875d85b95d1c56503
Parth-Chavda <parth18chavda@gmail.com>
2025-07-28 08:12:41 +0530

    path updated

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['user'])) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>NotiFyEd</title>
-  <link rel="icon" href="../../NotiFyEd/assets/img/WhatsApp Image 2025-07-27 at 13.07.31_5d8d0ddf.png" type="image/x-icon">
+  <link rel="icon" href="<?= urlof('./assets/img/WhatsApp Image 2025-07-27 at 13.07.31_5d8d0ddf.png')?>" type="image/x-icon">
 
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
@@ -19,6 +19,6 @@ if (!isset($_SESSION['user'])) {
 
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 
-   <link rel="stylesheet" href="../assets/css/nav.css">
+   <link rel="stylesheet" href="<?= urlof('./assets/css/nav.css')?>">
 
    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
